ac5b9deb26281a70399f2d7f181bb4a6ff07446d
Esme <esmereldaroses@gmail.com>
2024-07-15 19:10:25 +0100

    fixed tests for GET articles by ID & added GET All articles

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const { getTopics, getEndpoints } = require("./controllers/basic-controller");
-const { getArticleByID } = require("./controllers/article-controller");
+const { getArticleByID, getAllArticles } = require("./controllers/article-controller");
 
 
 app.get("/api/", getEndpoints);
@@ -11,14 +11,28 @@ app.get("/api/topics/", getTopics);
 
 app.get("/api/articles/:article_id/", getArticleByID);
 
+app.get("/api/articles/", getAllArticles);
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg });
+  } else next(err)
+})
   app.use((err, req, res, next) => {
-    if (err.code === "22P02" ) {
+    if (err.code === "22P02" || err.code ==="42703" ) {
       res.status(400).send({ msg: "Bad request" });
-    } 
-    else if (err.status && err.msg) {
-      res.status(err.status).send({ msg: err.msg });
-    } 
-    else res.status(500).send({ msg: "Internal server error" });
+    } else next(err)
   });
+
+  app.use((err, req, res, next) => {
+    if (err.code === "20000" ) {
+      res.status(404).send({ msg: "Not found" });
+    } else next(err)
+  });
+
+app.use(( err, req, res, next) => {
+    res.status(500).send({ msg: "Internal server error" });
+})
+
 
 module.exports = app;
--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,9 +1,17 @@
-const {fetchArticleByID}= require("../models/article-model")
+const {fetchArticleByID, fetchAllArticles}= require("../models/article-model")
 
-exports.getArticleByID =(request, response, next) => {
+exports.getArticleByID =( request, response, next) => {
     const articleID = request.params.article_id
     fetchArticleByID(articleID).then((article) => {
-        response.status(200).send({ article: article });
+   response.status(200).send({ article: article });
+    }).catch((err)=>{
+        next(err)
+    })
+}
+
+  exports.getAllArticles =( request, response, next) => {
+    fetchAllArticles().then((articles) => {
+        response.status(200).send({ articles: articles });
       }).catch((err)=>{
         next(err)
     })
