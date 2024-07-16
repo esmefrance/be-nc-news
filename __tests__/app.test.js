7984f007ba7b7cab3048b230ad3bba01270ae47f
Esme <esmereldaroses@gmail.com>
2024-07-16 12:02:52 +0100

    6:GET all comments linked to article

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -62,7 +62,7 @@ describe("GET articles by ID", () => {
         });
       });
   });
-  test("400: should return an error when invalid article ID is entered e.g. 'cat' ", () => {
+  test("400: error when invalid article ID is entered e.g. 'cat' ", () => {
     return request(app)
       .get("/api/articles/cat")
       .expect(400)
@@ -70,7 +70,7 @@ describe("GET articles by ID", () => {
         expect(body.msg).toBe("Bad request");
       });
   });
-  test("404 should return an error when an incorrect article ID is entered e.g.'123456789' ", () => {
+  test("404:error when an incorrect article ID is entered e.g.'123456789' ", () => {
     return request(app)
       .get("/api/articles/123456789")
       .expect(404)
@@ -81,33 +81,85 @@ describe("GET articles by ID", () => {
 });
 
 describe("GET all articles", () => {
-  test("should respond with array of article objects", () => {
+  test("respond with array of article objects", () => {
     return request(app)
       .get("/api/articles/")
       .expect(200)
-      .then((response) => {  
-        const articles = response.body.articles
-        articles.forEach((article) => {
-          expect(article).toEqual({
-            article_id: expect.any(Number),
-            title: expect.any(String),
-            topic: expect.any(String),
-            author: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number),
-            article_img_url: expect.any(String),
-            comment_count: expect.any(String)
+      .then((response) => {
+        const articles = response.body.articles;
+        if (articles.length > 0) {
+          articles.forEach((article) => {
+            expect(article).toEqual({
+              article_id: expect.any(Number),
+              title: expect.any(String),
+              topic: expect.any(String),
+              author: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+              comment_count: expect.any(String),
+            });
           });
-        });
+        }
       });
   });
-  test("responds with the articles ordered by created_date descending by default",()=>{
+  test("responds with the articles ordered by created_date descending by default", () => {
     return request(app)
-    .get("/api/articles/")
-    .expect(200)
-    .then((response) => {  
-        const articles = response.body.articles
-      expect(articles).toBeSortedBy( 'created_at', { descending: true})
-  })
+      .get("/api/articles/")
+      .expect(200)
+      .then((response) => {
+        const articles = response.body.articles;
+        expect(articles).toBeSortedBy("created_at", { descending: true });
+      });
+  });
 });
+
+describe("GET all article comments", () => {
+  test("respond with all comments for a specific article", () => {
+    return request(app)
+      .get("/api/articles/9/comments")
+      .expect(200)
+      .then((response) => {
+        const comments = response.body.comments;
+        if (comments.length > 0) {
+          comments.forEach((comment) => {
+            expect(comment).toEqual({
+              comment_id: expect.any(Number),
+              votes: expect.any(Number),
+              created_at: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+              article_id: 9,
+            });
+          });
+        }
+      });
+  });
+  test('comments should be sorted by "created_at" descending', () => {
+    return request(app)
+      .get("/api/articles/9/comments")
+      .expect(200)
+      .then((response) => {
+        const comments = response.body.comments;
+        expect(comments).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+  test('should return an empty array if the article exists but there are no comments', () => {
+    return request(app)
+      .get("/api/articles/2/comments")
+      .expect(200)
+      .then((response) => {
+        const comments = response.body.comments;
+        expect(comments).toEqual([]);
+      });
+  });
+  test("404:error when an incorrect article ID is entered e.g.'123456789' ", () => {
+    return request(app)
+      .get("/api/articles/123456789/comments")
+      .expect(404)
+      .then(({ body }) => {
+
+        expect(body.msg).toBe("Not found");
+      });
+  });
 });
