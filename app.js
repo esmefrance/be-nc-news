7d3001d543b8221acbc4bc0257f3bccf0137b16a
Esme <esmereldaroses@gmail.com>
2024-07-17 14:14:05 +0100

    GET articles and sort

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.delete("/api/comments/:comment_id/", deleteComment);
 
 app.get("/api/users/", getUsers);
 
-app.use((request, response, next) => {
+app.use(( request, response, next) => {
   response.status(404).send({ msg: "Not found" });
   next();
 });
@@ -45,13 +45,14 @@ app.use((err, req, res, next) => {
   } else next(err);
 });
 app.use((err, req, res, next) => {
-  if (err.code === "22P02" || err.code === "42703" || err.code === "23502") {
+
+  if (err.code === "22P02" || err.code === "42703" || err.code === "23502" ) {
     res.status(400).send({ msg: "Bad request" });
   } else next(err);
 });
 
 app.use((err, req, res, next) => {
-  if (err.code === "20000" || err.code === "23503") {
+  if (err.code === "20000" || err.code === "23503" || err.code === "42601") {
     res.status(404).send({ msg: "Not found" });
   } else next(err);
 });
--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -10,7 +10,8 @@ exports.getArticleByID =( request, response, next) => {
 }
 
   exports.getAllArticles =( request, response, next) => {
-    fetchAllArticles().then((articles) => {
+    const {sort_by, order} = request.query
+    fetchAllArticles(sort_by,order).then((articles) => {
         response.status(200).send({ articles: articles });
       }).catch((err)=>{
         next(err)
